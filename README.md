6b61424136589be625ad0e0f6108af82c01ea302
zyongtai <zyongtai@umich.edu>
2020-09-18 18:48:20 +0800

    init

--- README.md
+++ README.md
@@ -1,2 +1,17 @@
 # Zoom_transcript_HTML_to_vtt
+
 Convert Zoom transcript from HTML text to vtt format
+
+## Usage
+
+1. Press ``F12`` in the Zoom video window, click ``Elements`` tab
+
+2. Find the ``<ul>`` label which contains ``aria-label="Audio Transcript List"``
+
+3. Copy the whole section that is contained by that label
+
+4. Paste the text into a file called ``transcript.txt`` in the same directory as ``transcript.py``
+
+5. Run ``transcript.py``
+
+6. ``formatted_transcript.transcript.vtt`` in the same directory is the vtt transcript file
---
+++ transcript.py
@@ -0,0 +1,59 @@
+import re
+
+
+def filter_tags(htmlstr):
+    re_cdata = re.compile('//<!\[CDATA\[[^>]*//\]\]>', re.I)  # 匹配CDATA
+    re_script = re.compile('<\s*script[^>]*>[^<]*<\s*/\s*script\s*>', re.I)  # Script
+    re_style = re.compile('<\s*style[^>]*>[^<]*<\s*/\s*style\s*>', re.I)  # style
+    re_br = re.compile('<br\s*?/?>')  # 处理换行
+    re_h = re.compile('</?\w+[^>]*>')  # HTML标签
+    re_comment = re.compile('<!--[^>]*-->')  # HTML注释
+    blank_line = re.compile('\n+')
+
+    # 过滤匹配内容
+    s = re_cdata.sub('', htmlstr)  # 去掉CDATA
+    s = re_script.sub('', s)  # 去掉SCRIPT
+    s = re_style.sub('', s)  # 去掉style
+    s = re_br.sub('\n', s)  # 将br转换为换行
+    s = re_h.sub('', s)  # 去掉HTML 标签
+    s = re_comment.sub('', s)  # 去掉HTML注释
+    # s = blank_line.sub('\n', s)  # 去掉多余的空行
+
+    return s
+
+
+fi = open("transcript.txt", "r+", encoding='utf-8')
+try:
+    file_context = fi.read()
+    # file_context是一个string，读取完后，就失去了对test.txt的文件引用
+    # file_context = open(file).read().splitlines()
+    # file_context是一个list，每行文本内容是list中的一个元素
+finally:
+    fi.close()
+
+i = 1
+while file_context.find("<span data-v-4aeb73b8=\"\" class=\"time\">") != -1:
+    file_context = file_context.replace("<span data-v-4aeb73b8=\"\" class=\"time\">", str("\n\n" + str(i) + "\n"), 1)
+    i = i + 1
+file_context = file_context.replace("<span data-v-4aeb73b8=\"\" class=\"text\">", "\n")
+file_context = filter_tags(file_context)
+
+fo = open("formatted_transcript.transcript.vtt", "w+", encoding='utf-8')
+fo.write(file_context)
+fo.close()
+
+fo_c = open("formatted_transcript.transcript.vtt", "r+", encoding='utf-8')
+fo_list = fo_c.readlines()
+fo_c.close()
+
+fo_list[0] = "WEBVTT\n"
+i = 3
+while i + 4 < len(fo_list):
+    fo_list[i] = fo_list[i].replace('\n', '') + ".500 --> " + fo_list[i + 4].replace('\n', '') + ".501\n"
+    i = i + 4
+
+fo_list[i] = fo_list[i].replace('\n', '') + ".500 --> 01:30:00.520\n"
+
+fo_c = open("formatted_transcript.transcript.vtt", "w+", encoding='utf-8')
+fo_c.writelines(fo_list)
+fo_c.close()
